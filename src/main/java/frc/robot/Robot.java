dd266f8726134a1b140705180fe4fedbe720848c
joyadomari-chun24 <joyadomari-chun24@punahou.edu>
2024-03-19 16:05:42 -1000

    Slightly dubious AprilTag alignment detection code
    This needs some peer review before it gets merged for sure.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -37,6 +37,11 @@ public class Robot extends TimedRobot {
 
   private SendableChooser<Command> auto;
 
+  //Apriltag variables
+  private double tagCenterX;
+  private double tagCenterY;
+  private int tagId;
+
   // Color Sensor Code
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
@@ -54,7 +59,10 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
 
-    CameraServer.startAutomaticCapture(0);
+    CameraServer.startAutomaticCapture(0); //Starts capture for camera 0 (this one is purely video, no processing)
+    var visionThread = new Thread(this::apriltagVisionThreadProcessor); //Creates thread
+    visionThread.setDaemon(true); //Specifies that this thread is a daemon
+    visionThread.start(); //Starts the thread
 
     auto = new SendableChooser<>();
 
@@ -176,5 +184,115 @@ public class Robot extends TimedRobot {
   public void testPeriodic() {
     Shooter shooter = new Shooter();
     shooter.runShooter();
+  }
+
+  public double isTagInRange()
+  {
+    //Check if the tag is a speaker tag, then if it is an amp tag, then if it is a trap tag
+    if (tagId == 8 || tagId == 4)
+    {
+      if (tagCenterX < xSpeakerFarLimit && tagCenterX > xSpeakerCloseLimit && tagCenterY < ySpeakerUpperLimit && tagCenterX > ySpeakerLowerLimit)
+        return true;
+    }
+    else if (tagId == 5 || tagId == 6)
+    {
+      if (tagCenterX < xAmpFarLimit && tagCenterX > xAmpCloseLimit && tagCenterY < yAmpUpperLimit && tagCenterX > yAmpLowerLimit)
+        return true;
+    }
+    else if (tagId >= 11 && tagId <= 16)
+    {
+      if (tagCenterX < xTrapFarLimit && tagCenterX > xTrapCloseLimit && tagCenterY < yTrapUpperLimit && tagCenterX > yTrapLowerLimit)
+        return true;
+    }
+    return false;
+  }
+
+  /** 
+   * Processor code for april tags
+  */
+  private void apriltagVisionThreadProcessor() {
+
+    AprilTagDetector detector = new AprilTagDetector();
+
+    detector.addFamily("tag16h5", 0); // Specifies the type of april tags to look for
+
+    // Get the UsbCamera from CameraServer
+    UsbCamera camera1 = CameraServer.startAutomaticCapture(1);
+
+    // Set the resolution
+    camera1.setResolution(640, 480);
+
+    // Get a CvSink. This will capture Mats from the camera
+    CvSink cvSink = CameraServer.getVideo();
+    // Setup a CvSource. This will send images back to the Dashboard
+    CvSource outputStream = CameraServer.putVideo("detect", 640, 480);
+
+    // Mats are very memory expensive. Lets reuse this Mat ().
+    Mat mat = new Mat();
+    Mat grayMat = new Mat();
+
+    // Instantiate colors and list of tags
+    ArrayList<Integer> tags = new ArrayList<>();
+    Scalar outlineColor = new Scalar(0, 255, 0);
+    Scalar xColor = new Scalar(0, 0, 255);
+
+    // This cannot be 'true'. The program will never exit if it is. This
+    // lets the robot stop this thread when restarting robot code or
+    // deploying.
+    while (!Thread.interrupted()) {
+      // Tell the CvSink to grab a frame from the camera and put it
+      // in the source mat.  If there is an error notify the output.
+      if (cvSink.grabFrame(mat) == 0) {
+        // Send the output the error.
+        outputStream.notifyError(cvSink.getError());
+        // skip the rest of the current iteration
+        continue;
+      }
+
+      // Convert video from rgb to gray
+      Imgproc.cvtColor(mat, grayMat, Imgproc.COLOR_RGB2GRAY); 
+
+      boolean importantTags = false; // boolean that's used to check if any important IDs were found
+      AprilTagDetection[] detections = detector.detect(grayMat);
+      tags.clear(); //Clear list of detected tags from the last loop of code
+      // Loop through all the camera's detections and get their IDs and draw a rectangle around them on the camera
+      for (AprilTagDetection detection : detections) {
+
+        tags.add(detection.getId());
+
+        if(detection.getId() == 8 || (detection.getId() => 4 && detection.getID() <= 6) || (detection.getId() >= 11 && detection.getId() <= 16)) {
+          hasImportantTags = true;
+          tagCenterX = detection.getCenterX;
+          tagGenterY = detection.getCenterY;
+          tagId = detection.getId();
+          importantTags = true;
+        }
+
+        for (var i = 0; i <= 3; i++) {
+          var j = (i + 1) % 4;
+          var pt1 = new Point(detection.getCornerX(i), detection.getCornerY(i));
+          var pt2 = new Point(detection.getCornerX(j), detection.getCornerY(j));
+          Imgproc.line(mat, pt1, pt2, outlineColor, 2);
+        }
+
+        cx = detection.getCenterX();
+        cy = detection.getCenterY();
+        var ll = 10;
+
+        Imgproc.line(mat, new Point(cx - ll, cy), new Point(cx + ll, cy), xColor, 2);
+        Imgproc.line(mat, new Point(cx, cy - ll), new Point(cx, cy + ll), xColor, 2);
+        Imgproc.putText(mat, Integer.toString(detection.getId()), new Point (cx + ll, cy), Imgproc.FONT_HERSHEY_SIMPLEX, 1, xColor, 3);
+      }
+
+      if (!importantTags) {
+        tagCenterX = 0;
+        tagCenterY = 0;
+      }
+
+      SmartDashboard.putString("tag", tags.toString());
+      // Give the output stream a new image to display
+      outputStream.putFrame(mat);
+    }
+    detector.close();
   }
 }
